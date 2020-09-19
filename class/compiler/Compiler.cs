306db6127c5cadcd22423ade03cbec524bd57122
Maite de la Roca <delaroca.estrada21@gmail.com>
2020-09-18 23:19:39 -0600

    CLI setup

--- class/compiler/Compiler.cs
+++ class/compiler/Compiler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Cursed_compiler
 {
@@ -6,7 +7,95 @@ namespace Cursed_compiler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int parameters = args.Length;
+            switch(parameters){
+                case 0:
+                    Console.WriteLine("Texto ayuda");
+                    break;
+
+                case 3:
+                    Console.WriteLine("filename: " + args[0].ToString());
+                    switch(args[1]){
+                        case "-o":
+                            Console.WriteLine("Output en file: " + args[2].ToString());
+                            break;
+                        case "-target":
+                            stage(args[1],args[2]);
+                            break;
+
+                        case "-opt":
+                            switch(args[2]){
+                                case "constant":
+                                    Console.WriteLine("constant optimizing");
+                                    break;
+                                case "algebraic":
+                                    Console.WriteLine("algebraic optimization");
+                                    break;
+                                default:
+                                    Console.WriteLine("Cursed optimization >:)  F");
+                                    break;
+                            }
+                            break;
+                            case "-debug":
+                                stage(args[1],args[2]);
+                                break;                        
+                        default:
+                            Console.WriteLine("Cursed flag");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Jaja mucho o poco texto");
+                    break;
+            }
+            void stage(String flag, String myArg){
+                String message;
+                if(flag=="-target"){
+                    message="stage";
+                }else{
+                    message="debugging";
+                }
+                switch(myArg){
+                    case "scan":
+                        Console.WriteLine(message + ": scanning");
+                        break;
+                    case "parse":
+                        Console.WriteLine(message + ": scanning");
+                        Console.WriteLine(message + ": parsing");
+                        break;
+                    case" ast":
+                        Console.WriteLine(message + ": scanning");
+                        Console.WriteLine(message + ": parsing");
+                        Console.WriteLine(message + ": ast");
+                        break;
+                    case" semantic":
+                        Console.WriteLine(message + ": scanning");
+                        Console.WriteLine(message + ": parsing");
+                        Console.WriteLine(message + ": ast");
+                        Console.WriteLine(message + ": semantic");
+                        break;
+                    case" irt":
+                        Console.WriteLine(message + ": scanning");
+                        Console.WriteLine(message + ": parsing");
+                        Console.WriteLine(message + ": ast");
+                        Console.WriteLine(message + ": semantic");
+                        Console.WriteLine(message + ": irt");
+                        break;
+                    case "codegen":
+                        Console.WriteLine(message + ": scanning");
+                        Console.WriteLine(message + ": parsing");
+                        Console.WriteLine(message + ": ast");
+                        Console.WriteLine(message + ": semantic");
+                        Console.WriteLine(message + ": irt");
+                        Console.WriteLine(message + ": code generation");
+
+                        break;
+                    default:
+                        Console.WriteLine("Cursed stage >:)");
+                        break;
+                }
+                
+            }
         }
     }
 }
---
+++ class/opt/Algebraic.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Cursed_compiler
+{
+    class Algebraic
+    {
+        
+    }
+}
